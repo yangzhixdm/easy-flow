331a7e476cad3bcce128e1b72ab92cba138b46c0
yangzhixdm <helloyangzhi@foxmail.com>
2020-11-17 16:57:10 +0800

    feat: add event module

--- src/event/index.js
+++ src/event/index.js
@@ -1,5 +1,24 @@
 class Event {
+  constructor () {
+    this.hooks = new Map()
+  }
 
+  on (type, handler) {
+    if (this.hooks[type]) {
+      this.hooks[type].push(handler)
+    } else {
+      this.hooks[type] = [handler]
+    }
+  }
+
+  emit (type, ...args) {
+    if (this.hooks[type]) {
+      let handler = this.hooks[type]
+      for (let i = 0; i < handler.length; i++) {
+        handler[i].call(this, args)
+      }
+    }
+  }
 }
 
 export default Event
